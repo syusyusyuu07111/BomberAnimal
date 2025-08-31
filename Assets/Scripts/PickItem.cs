ead162ea60921f6abec677c5c469a7f671b6fcd4
syusyusyuu07111 <syuji0007@outlook.jp>
2025-08-31 13:07:38 +0900

    キャラとアイテムの位置取得

---
+++ Assets/Scripts/PickItem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PickItem : MonoBehaviour
+{
+    [Header("参照")]
+    public Transform Charecter;//キャラの位置
+    public Transform Item;//アイテムの位置
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
