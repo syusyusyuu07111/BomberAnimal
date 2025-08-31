11795af48d888b8cdde8e76af4ef66c5bdc6a3df
syusyusyuu07111 <syuji0007@outlook.jp>
2025-08-31 16:25:29 +0900

    拾ったテキストを表示

--- Assets/Scripts/PickItem.cs
+++ Assets/Scripts/PickItem.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PickItem : MonoBehaviour
@@ -5,9 +6,11 @@ public class PickItem : MonoBehaviour
     [Header("参照")]
     public Transform Character;//キャラの位置
     public Transform Item;//アイテムの位置
+    public float pickdistance = 1.5f;//拾える距離
+    public TextMeshProUGUI TMP;
 
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (Item == null || Character == null) return;
@@ -15,10 +18,25 @@ public class PickItem : MonoBehaviour
             //距離を計算
             float distance = Vector3.Distance(Character.position, Item.position);
 
-            Debug.Log("キャラとアイテムの距離" + distance);
+           // Debug.Log("キャラとアイテムの距離" + distance);
+
+            if(distance<=pickdistance)
+            {
+
+                TMP.gameObject.SetActive(true);
+
+                Debug.Log("拾える距離");
+            }
+            if (distance >= pickdistance)
+            {
+
+
+                TMP.gameObject.SetActive(false);
+                Debug.Log("拾えない距離");
+            }
 
         }
-
+       
 
     }
 }
