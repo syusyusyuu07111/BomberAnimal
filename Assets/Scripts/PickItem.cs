016b8acf09e57066c64686058d66aa3a5187fcd2
syusyusyuu07111 <syuji0007@outlook.jp>
2025-08-31 13:16:55 +0900

    キャラとアイテムの距離計算

--- Assets/Scripts/PickItem.cs
+++ Assets/Scripts/PickItem.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class PickItem : MonoBehaviour
 {
     [Header("参照")]
-    public Transform Charecter;//キャラの位置
+    public Transform Character;//キャラの位置
     public Transform Item;//アイテムの位置
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Item == null || Character == null) return;
+        {
+            //距離を計算
+            float distance = Vector3.Distance(Character.position, Item.position);
+
+            Debug.Log("キャラとアイテムの距離" + distance);
+
+        }
+
+
     }
 }
