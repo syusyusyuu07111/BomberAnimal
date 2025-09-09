15c08f2f3f70f06ed75c9ce98600d4e984ab51ef
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-09 18:10:12 +0900

    inputaction move取得

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,24 @@ public class PlayerController : MonoBehaviour
 {
     public InputSystem_Actions inputActions;
     public Rigidbody rb;
-    public Vector2 input;
+    public Vector2 MoveInput;
 
     [SerializeField] public float MoveSpeed =5f; //移動スピード
+    [SerializeField] public Transform CameraTransform;//カメラの向き取得
     void Start()
     {
         inputActions = new InputSystem_Actions();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnable()
     {
+        inputActions.Player.Enable();
+    }
+    public void Update()
+    {
+        Vector2 MoveInput = inputActions.Player.Move.ReadValue<Vector2>();//inputaction move取得
 
     }
+
 }
