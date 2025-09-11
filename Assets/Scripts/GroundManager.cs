a04bd53b517605a3b806d7c568e1811492e96a96
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-11 15:53:29 +0900

    ブロック揺らす数値設定

--- Assets/Scripts/GroundManager.cs
+++ Assets/Scripts/GroundManager.cs
@@ -5,12 +5,16 @@ public class GroundManager : MonoBehaviour
 {
     [SerializeField] public float GroundHP;
     [SerializeField] public float FallCount=5.0f;
+    public float NoiseOffset;
     void Start()
     {
 
     }
 
-
+    public void SetNoise()
+    {
+        NoiseOffset = UnityEngine.Random.Range(0f, 256f);
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
