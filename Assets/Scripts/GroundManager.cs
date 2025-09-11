3976b229c1a2bf381fe4aa2b43316f901a4be698
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-11 16:47:19 +0900

    角度計算

--- Assets/Scripts/GroundManager.cs
+++ Assets/Scripts/GroundManager.cs
@@ -5,28 +5,49 @@ public class GroundManager : MonoBehaviour
 {
     [SerializeField] public float GroundHP;
     [SerializeField] public float FallCount=5.0f;
+    [SerializeField] public float NoiseSpeed;
+
+    Vector3 Basepos;//基準の位置
+
+    public bool TouchFlag;
+
     public float NoiseOffset;
     void Start()
     {
+        TouchFlag = false;
+        Basepos = transform.position;//基準の位置
+
 
     }
-
     public void SetNoise()
     {
         NoiseOffset = UnityEngine.Random.Range(0f, 256f);
     }
+    private void Update()
+    {
+        if(TouchFlag)//ブロックを揺らす
+        {
+            //角度計算 θ(t) = ω t + φ
+            float Angle = Time.time * NoiseSpeed + NoiseOffset;
+
+        }
+    }
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             StartCoroutine(FallBlock(FallCount));
+            TouchFlag = true;
+
         }
 
     }
     private IEnumerator FallBlock(float delay)
     {
         yield return new WaitForSeconds(delay);
-        Destroy(this.gameObject);
+        //Destroy(this.gameObject);
     }
 
 }
