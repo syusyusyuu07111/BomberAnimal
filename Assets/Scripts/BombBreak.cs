bd137113b5bfcaaf29802dc3921672b47e8323a3
syusyusyuu07111 <syuji0007@outlook.jp>
2025-09-16 19:02:23 +0900

    爆発のときのmass修正

--- Assets/Scripts/BombBreak.cs
+++ Assets/Scripts/BombBreak.cs
@@ -6,6 +6,7 @@ public class BombBreak : MonoBehaviour
 
     [Header("爆発エフェクト")]
     public GameObject BombFirePrefab;
+    public float OriginMass;
     private void OnEnable()
     {
         StartCoroutine("Explosion");
@@ -15,15 +16,25 @@ public class BombBreak : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
         Instantiate(BombFirePrefab, transform.transform.position, Quaternion.identity);
 
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 5f);
+        Collider[] colliders = Physics.OverlapSphere(transform.position,3.0f);
         foreach(Collider collider in colliders)
         {
             Rigidbody rb = collider.GetComponent<Rigidbody>();
             if(rb!=null)
+                //massをへらして範囲内の足場を吹っ飛ばす=================================================================================
             {
-                rb.AddExplosionForce(250f, transform.position, 5f, 0f, ForceMode.Impulse);
+                OriginMass = rb.mass;
+                rb.mass = 1.0f;
+                rb.AddExplosionForce(100f, transform.position, 0.1f, 0f, ForceMode.Impulse);
+                StartCoroutine(ReturnoriginMass(rb, OriginMass, 0.0001f));
             }
         }
+    }
+    //爆発の瞬間にへらしてたmassを元に戻す==================================================================================================--
+    IEnumerator ReturnoriginMass(Rigidbody rb, float originMass, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        rb.mass = originMass;
         Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,18 +1,13 @@
+using System.Collections;
 using UnityEngine;
-
-
-
-
+using UnityEngine.SceneManagement;
 //敵AIの挙動=======================================================================================================================================
 public class EnemyAI : MonoBehaviour
 {
     public Transform Player;
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    public Vector3 SafePos;
+    [SerializeField] public float Speed=10.0f;
+    public Vector3 MovePosition;
     void Update()
     {
         //近くのオブジェクトを取得して状態をしらべる---------------------------------------------------------------------------------------------------
@@ -22,11 +17,29 @@ public class EnemyAI : MonoBehaviour
             Debug.Log("敵の近くのオブジェクト"+collider.name);
             //近くのオブジェクトのrenderer取得して色をしらべる------------------------------------------------------------------------------------------
             Renderer rend = collider.GetComponent<Renderer>();
-            if(collider!=null)
+            if(rend!=null)
             {
                 Color color = rend.material.color;
                 Debug.Log("周りのオブジェクトの色:"+color);
+                SafePos = collider.transform.position;
+                //安全なブロックを見つけてそこに移動する------------------------------------------------------------------------------------------------------
+                if (color == Color.white)
+                {
+                    float step = Speed * Time.deltaTime;
+                    MovePosition = new Vector3(SafePos.x, transform.position.y, SafePos.z);
+                    transform.position = Vector3.MoveTowards(transform.position,MovePosition,step);
+                    //transform.position = Vector3.MoveTowards(SafePos.x,transform.position.y,SafePos.z);
+                }
             }
+
+        }
+    }
+    //敵がデッドゾーンに行ったらリザルト画面に遷移する
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("DeadZone"))
+        {
+            SceneManager.LoadScene("GameOver");
         }
     }
 }
